6712446fc761e26779f8bb54a0d3738cc739db23
wzg <wzg0312@163.com>
2018-09-03 14:38:20 +0800

    add p2p named file

---
+++ p2p/p2p.go
@@ -0,0 +1,27 @@
+package p2p
+
+import (
+	"github.com/linkchain/common/util/log"
+	_ "net"
+)
+
+type Service struct {
+}
+
+func (s *Service) Init(i interface{}) bool {
+	log.Info("p2p service init...")
+	return true
+}
+
+func (s *Service) Start() bool {
+	log.Info("p2p service start...")
+	return true
+}
+
+func (s *Service) Stop() {
+	log.Info("p2p service stop...")
+}
+
+func (s *Service) Foo() {
+	log.Info("FOOO")
+}
