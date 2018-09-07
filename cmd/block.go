708b9d4c863ee61e78de968644fae06f84b0e3e3
BananaLF <864685021@qq.com>
2018-09-07 16:54:48 +0800

    update getblockbyheight cmd

--- cmd/block.go
+++ cmd/block.go
@@ -4,11 +4,15 @@ package cmd
 import (
 	"github.com/spf13/cobra"
 	"github.com/linkchain/poa/poamanager"
+	"github.com/linkchain/common/util/log"
+	"strconv"
 )
 
 func init() {
 	RootCmd.AddCommand(mineCmd)
 	RootCmd.AddCommand(chainInfoCmd)
+	RootCmd.AddCommand(blockCmd)
+	blockCmd.AddCommand(heightCmd)
 }
 
 var mineCmd = &cobra.Command{
@@ -35,4 +39,37 @@ var loadChainCmd = &cobra.Command{
 		poamanager.GetManager().ChainManager.UpdateChain()
 	},
 }
+
+var blockCmd = &cobra.Command{
+	Use:   "block",
+	Short: "block command",
+}
+
+var heightCmd = &cobra.Command{
+	Use:   "height",
+	Short: "get a block by height",
+	Run: func(cmd *cobra.Command, args []string) {
+		example := []string{"example","block height 0"}
+		if len(args) != 1 {
+			log.Error("getblockbyheight","error","please input height",example[0],example[1])
+			return
+		}
+
+		height,error := strconv.Atoi(args[0])
+		if error != nil {
+			log.Error("getblockbyheight ","error",error,example[0],example[1])
+			return
+		}
+
+		if uint32(height) > poamanager.GetManager().ChainManager.GetBestBlock().GetHeight() || height < 0{
+			log.Error("getblockbyheight ","error","height is out of range",example[0],example[1])
+			return
+		}
+		log.Info("block","data",poamanager.GetManager().ChainManager.GetBlockByHeight(uint32(height)))
+	},
+}
+
+
+
+
 
